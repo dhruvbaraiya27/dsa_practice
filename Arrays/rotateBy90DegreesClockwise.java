bd58208ffd89d35d82bba500a2b8fdb75c1f014f
dhruvbaraiya27 <baraiya.d@northeastern.edu>
2025-01-22 17:53:33 -0500

    Create rotateBy90DegreesClockwise.java

---
+++ Arrays/rotateBy90DegreesClockwise.java
@@ -0,0 +1,25 @@
+class GFG {
+    
+    static void rotate(int mat[][]) {
+        // Code Here
+      int rows=mat.length;
+      int cols=mat[0].length;
+      
+      for(int i=0;i<rows/2;i++)
+      {
+          int temp[] = mat[i];
+          mat[i]=mat[rows-i-1];
+          mat[rows-i-1]=temp;
+      }
+      for(int i=0;i<rows;i++)
+      {
+          for(int j=i;j<cols;j++)
+          {
+              int temp = mat[i][j];
+              mat[i][j]=mat[j][i];
+              mat[j][i]=temp;
+          }
+      }
+      
+    }
+}
