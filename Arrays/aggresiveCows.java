e756a11dc78b02d52dbbf2523f7b7e2a293d10b8
dhruvbaraiya27 <baraiya.d@northeastern.edu>
2025-02-08 18:53:42 -0500

    Update aggresiveCows.java

--- Arrays/aggresiveCows.java
+++ Arrays/aggresiveCows.java
@@ -7,6 +7,7 @@ import java.util.*;
 class GFG {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         int tc = Integer.parseInt(br.readLine().trim());
 
         while (tc-- > 0) {
