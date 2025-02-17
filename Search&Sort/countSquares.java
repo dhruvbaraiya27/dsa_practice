27a51b49d2dc69ee2c73b2cd289df734ecd9d542
dhruvbaraiya27 <baraiya.d@northeastern.edu>
2025-02-17 18:34:48 -0500

    Create countSquares.java

---
+++ Search&Sort/countSquares.java
@@ -0,0 +1,26 @@
+class Solution {
+    static int countSquares(int n) {
+        // code here
+        int low=0;
+        int high=n-1;
+        
+        int ans=0;
+        
+        while(low<=high)
+        {
+            int mid = low+(high-low)/2;
+            
+            if((long) mid * mid <n)
+            {
+                ans =mid;
+                low=mid+1;
+            }
+            else
+            {
+                high=mid-1;
+            }
+        }
+        return ans;
+        
+    }
+}
