233ed71ae3a1ca394ba6ff9a1455d4b6c184919b
dhruvbaraiya27 <baraiya.d@northeastern.edu>
2025-02-17 20:10:22 -0500

    Create 4Sum.java

---
+++ Search&Sort/4Sum.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] arr, int target) {
+        // code here
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(arr);
+        int n=arr.length;
+        for(int i=0;i<n-3;i++)
+        {
+            if(i>0 && arr[i]==arr[i-1]) continue;
+            for(int j=i+1;j<n-2;j++)
+            {
+                if(j > i+1 && arr[j]==arr[j-1]) continue;
+                int l=j+1;
+                int r=n-1;
+                
+                while(l<r)
+                {
+                    long sum = arr[i]+arr[j]+arr[l]+arr[r];
+                    
+                    if(sum==target)
+                    {
+                        list.add(new ArrayList<>(Arrays.asList(arr[i],arr[j],arr[l],arr[r])));
+                        
+                        while(l<r && arr[l]==arr[l+1]) l++;
+                        while(l<r && arr[r]==arr[r-1]) r--;
+                        
+                        l++;
+                        r--;
+                    }
+                    else if(sum<target) l++;
+                    else r--;
+                    
+                }
+                
+            }
+        }
+        return list;
+    }
+}
