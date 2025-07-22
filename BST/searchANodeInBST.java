7d512de3c42314942ccee6f45155b13b301f04e4
Dhruv Baraiya <baraiya.d@northeastern.edu>
2025-07-22 18:13:25 -0400

    Create searchANodeInBST.java

---
+++ BST/searchANodeInBST.java
@@ -0,0 +1,16 @@
+class BST {
+    // Function to search a node in BST.
+    boolean search(Node root, int x) {
+        // Your code here
+        if(root==null)  return false;
+        Node temp=root;
+        while(temp!=null)  
+        {
+            if(temp.data ==x)   return true;
+            else if(temp.data > x)  temp=temp.left;
+            else    temp=temp.right;
+        }
+        return false;
+        
+    }
+}
