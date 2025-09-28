c1839755dbc4ef30a5f6626a4f68aea9764cb888
Dhruv Baraiya <baraiya.d@northeastern.edu>
2025-09-28 13:29:24 -0400

    Update CreatingAndPrinting.java

--- Graphs/CreatingAndPrinting.java
+++ Graphs/CreatingAndPrinting.java
@@ -1,7 +1,6 @@
 import java.util.*;
 public class Solution {
     public static int[][] printAdjacency(int n, int m, int[][] edges) {
-        // Write your code here.
         List<List<Integer>> adjList = new ArrayList<>();
         for(int i=0;i<n;i++){
             adjList.add(new ArrayList<>());
@@ -12,23 +11,23 @@ public class Solution {
             int v = edges[i][1];
             adjList.get(u).add(v);
             adjList.get(v).add(u);
-        }
 
-        for(List<Integer> list : adjList){
-            Collections.sort(list);
         }
 
         int[][] result = new int[n][];
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++) {
             List<Integer> neighbors = adjList.get(i);
+            Collections.sort(neighbors);
             result[i] = new int[neighbors.size() + 1];
-            result[i][0]=i;
+            result[i][0] = i;
             for(int j=0;j<neighbors.size();j++){
-                result[i][j+1] =neighbors.get(j); 
-            }
-
+                result[i][j+1] = neighbors.get(j);
+            } 
         }
         return result;
+
+        
+        
 
     }
 }
